215fe2d6362884dbb9554127c722143a749ca376
marziamsc <83905680+marziamsc@users.noreply.github.com>
2021-05-09 14:45:09 +0200

    Add files via upload

---
+++ SpaceGroove/js/functions.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+// PRELOADER	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+	}, 3000);
+	
+// SCROLL REVEAL
+	window.sr = new scrollReveal();
+
+// PARALLAX IMAGES 
+//	$('#pianeti').parallax("50%", 0.0);
+	  
+});
