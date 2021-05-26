06a04ae622b16389886c557f1268b6a0f2adb97f
marziamsc <83905680+marziamsc@users.noreply.github.com>
2021-05-27 00:50:43 +0200

    Delete SpaceGroove directory

--- SpaceGroove/js/functions.js
+++
@@ -1,9 +0,0 @@
-// SCROLL REVEAL
-$(document).ready(function() {
-	window.sr = new scrollReveal();
-});
-
-// REDIRECTION - SAFARI
-function redirection(destination){
-	window.location.href = "../index.php#" + destination;
-}
